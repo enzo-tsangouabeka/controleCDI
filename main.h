692ef9eaa50149be8efd1e46d2d3719609c3709d
etudiant <etudiant@bts.ciel.priv>
2025-04-29 16:30:28 +0200

    firstCommit

---
+++ main.h
@@ -0,0 +1,18 @@
+/**
+* @file main.h
+ * @brief Projet Application de gestion du CDI
+ * @author Enzo Tsangouabeka
+ * @version v1.0
+ * @date 29/04/2025
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+
+using namespace std;
+
+
+
+#endif //MAIN_H
